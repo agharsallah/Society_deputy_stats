caee95121383382a4e8bc0271b1a09f65b75cd23
abdou <aa.test20@gmail.com>
2016-12-23 14:25:35 +0100

    setting basic login

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+    const path = require('path');
+    const webpack = require('webpack');
+    const notifier = require('node-notifier');
+
+module.exports = {
+
+    entry:  [
+                './app/server/views/components/Index.js'
+                //'./app/server/views/admin-components/Index.js'
+            ],
+    
+    module: {
+            loaders:    [{
+                            test: /\.js$/,           
+                             exclude: /node_modules/,
+                            loaders: ['babel']
+                   
+                        }]
+    },
+
+    resolve: {
+                extensions: ['', '.js', '.jsx']
+    },
+
+    output: {
+                path:'./app/public/js/gulp/',
+                filename: 'simplex.js'
+                //filename: 'adminx.js'
+    }
+    
+
+};
